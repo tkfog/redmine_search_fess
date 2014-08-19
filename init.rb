f274c9a08bba4f45e1f7916f942e22bc767ffcb5
Takafumi Ogino <tk.ogino@gmail.com>
2014-08-19 23:44:41 +0900

    initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_search_fess do
+  name 'Redmine Fess Search plugin'
+  author 'tkfog (Takafumi Ogino)'
+  description 'Redmine Search plugin on repository using Fess'
+  version '0.0.1'
+  url 'https://github.com/tkfog/redmine_search_fess'
+  author_url 'https://github.com/tkfog/'
+end
